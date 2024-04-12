fa566deab77d0da44bc7b0c56efa9436d9370974
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-04-12 08:56:01 +0300

    boxes were created

--- board.js
+++ board.js
@@ -1,23 +1,14 @@
+
+
+
 function setup(){
-    createCanvas(650, 650,WEBGL);
+    createCanvas(560, 560);
+    initBoxes();
+    
 
 }
 function draw(){
 
-  
-    ambientLight(100, 60, 60);
-    pointLight(255, 255, 0, 0, -100, 100);
-    pointLight(255, 255, 255, 0, 10, 100);
-
-  
-
-    push()
-    fill("green")
-    rotateX(200);
-    cylinder(300,30,50,50)
-    pop()
-
-
-
-   
+    background("green")
+    creatBoxes()
 }
---
+++ box.js
@@ -0,0 +1,29 @@
+let boxes = [[],[],[],[],[]]
+
+const boxesRow = 1;
+const boxesColumn = 1;
+const boardSize = 5;
+
+function initBoxes(){
+    for (let i = 0; i < boardSize; i++){
+      for (let j = 0; j < boardSize; j++){
+        boxes[i][j]={
+          x: i*110 + 10,
+          y: j*110 + 10,
+          color: "rgb(163, 90, 42)",
+          side:100
+        }
+      }
+    }
+}
+
+function creatBoxes(){
+  for (let i = 0; i < boardSize; i++){
+    for (let j = 0; j < boardSize; j++){
+      fill(boxes[i][j].color)
+      rect(boxes[i][j].x,boxes[i][j].y,boxes[i][j].side,boxes[i][j].side)
+    }
+  }
+}
+
+
