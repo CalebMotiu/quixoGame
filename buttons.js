33589454cd0e54cee7c9bc9eb2acf2313defedfe
username <email addres>
2024-06-04 20:39:39 +0300

    buttons style

--- buttons.js
+++ buttons.js
@@ -29,6 +29,7 @@ function createNameInput() {
     name1inp.style('font-family', "'Courier New', Courier, monospace");
     name1inp.style('border-radius', '5px');
     name1inp.style('box-shadow', '2px 2px 5px rgba(0, 0, 0, 0.2)');
+
     name2inp.style('background-color', '#d3b7a1');
     name2inp.style('color', '#4a4846');
     name2inp.style('padding', '10px 20px');
@@ -37,6 +38,24 @@ function createNameInput() {
     name2inp.style('font-family', "'Courier New', Courier, monospace");
     name2inp.style('border-radius', '5px');
     name2inp.style('box-shadow', '2px 2px 5px rgba(0, 0, 0, 0.2)');
+   
+    name1bt.style('background-color', '#d3b7a1');
+    name1bt.style('color', '#4a4846');
+    name1bt.style('padding', '10px 20px');
+    name1bt.style('border', 'none');
+    name1bt.style('font-size', '16px');
+    name1bt.style('font-family', "'Courier New', Courier, monospace");
+    name1bt.style('border-radius', '5px');
+    name1bt.style('box-shadow', '2px 2px 5px rgba(0, 0, 0, 0.2)');
+    
+    name2bt.style('background-color', '#d3b7a1');
+    name2bt.style('color', '#4a4846');
+    name2bt.style('padding', '10px 20px');
+    name2bt.style('border', 'none');
+    name2bt.style('font-size', '16px');
+    name2bt.style('font-family', "'Courier New', Courier, monospace");
+    name2bt.style('border-radius', '5px');
+    name2bt.style('box-shadow', '2px 2px 5px rgba(0, 0, 0, 0.2)');
     name1bt.mousePressed(name1);
     name2bt.mousePressed(name2);
     resetbt = createButton("RESET");
