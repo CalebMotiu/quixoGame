5e6d259fb42c1b664aa8d6997cf3c5545cb98c67
SandruEzechiel <146414741+SandruEzechiel@users.noreply.github.com>
2024-06-04 20:54:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ function setup() {
     computerbt = createButton("Jucator vs Calculator");
     humanbt.position(canvasWidth / 2 - 50, canvasHeight / 3);
     computerbt.position(canvasWidth / 2 - 60, canvasHeight / 1.5);
+
+    humanbt.style('background-color', '#d3b7a1');
+    humanbt.style('color', '#4a4846');
+    humanbt.style('padding', '10px 20px');
+    humanbt.style('border', 'none');
+    humanbt.style('font-size', '16px');
+    humanbt.style('font-family', "'Courier New', Courier, monospace");
+    humanbt.style('border-radius', '5px');
+    humanbt.style('box-shadow', '2px 2px 5px rgba(0, 0, 0, 0.2)');
+
+    computerbt.style('background-color', '#d3b7a1');
+    computerbt.style('color', '#4a4846');
+    computerbt.style('padding', '10px 20px');
+    computerbt.style('border', 'none');
+    computerbt.style('font-size', '16px');
+    computerbt.style('font-family', "'Courier New', Courier, monospace");
+    computerbt.style('border-radius', '5px');
+    computerbt.style('box-shadow', '2px 2px 5px rgba(0, 0, 0, 0.2)');
+    
     humanbt.mousePressed(human);
     computerbt.mousePressed(computer);
 }
