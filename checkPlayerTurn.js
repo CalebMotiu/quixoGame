7da1ec01e5b6de517641ae63b4d76a7dcd958fdd
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-06-04 20:08:15 +0300

    resolved errors

--- checkPlayerTurn.js
+++
@@ -1,6 +0,0 @@
-let currentPlayerText = "x";
-
-function switchTurn() {
-    currentPlayerText = currentPlayerText === "x" ? "o" : "x";
-    currentPlayer = currentPlayer === name1 ? name2 : name1;
-}
